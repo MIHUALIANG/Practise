961ac21558f06f0a228ed6f49af86df8dc5cc2c5
MIHUALIANG <2628966994@qq.com>
2025-10-14 01:46:05 +0800

    First commit

---
+++ Native/vue-native/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+  <n-button>naive-ui</n-button>
+  <n-space>
+    <n-button>Default</n-button>
+    <n-button type="tertiary"> Tertiary </n-button>
+    <n-button type="primary"> Primary </n-button>
+    <n-button type="info"> Info </n-button>
+    <n-button type="success"> Success </n-button>
+    <n-button type="warning"> Warning </n-button>
+    <n-button type="error"> Error </n-button>
+  </n-space>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
